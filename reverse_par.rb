4d7f7a58084ca31df6363ca75eb3e9380f4bf9c1
Bilal Syed Hussain <bhterra@gmail.com>
2011-09-08 22:29:17 +0100

    reverse_par fixes

--- reverse_par.rb
+++ reverse_par.rb
@@ -1,5 +1,9 @@
+#!/usr/bin/env ruby -wKU
 (puts "#{File.basename $0} filename\nPrints the file with pars reversed"; exit) unless ARGV.length == 1
-file = IO.read(ARGV[0])
+path = ARGV[0]
+# path = "~/a.txt"
+file = IO.read(File.expand_path path)
+file.gsub!(/\r\n?/, "\n")
 lines = file.split(/\n\n/)
 
 lines.reverse.each { |e| puts e;puts }
