aa77df76980fe7ca33f33d1b0695d1ad81ab8dc6
Bilal Syed Hussain <bhterra@gmail.com>
2012-04-29 15:11:52 +0100

    misc

--- tracks.rb
+++ tracks.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/env ruby19 -wKU
-# Prints out the infomation about playcounts
+# Prints out the infomation about playcounts from a specifed YAML file
+# Bilal Syed Hussain
+
 require "yaml"
 require "pp"
+require 'trollop'
 
-SORT_NAME   =-> k,v { k }
-SORT_TOTAL  =-> k,v { -v[:total_tracks]}
-SORT_CREATE =-> k,v { v[:creation]}
-SORT_PLAYS  =-> k,v { -v[:total_count]}
-SORT_PLAYED =-> k,v { -v[:played].to_f/v[:total_tracks].to_f}
+Sorts = {
+	name:     -> k,v { k }                                    ,
+	tracks:   -> k,v { -v[:total_tracks]}                     ,
+	creation: -> k,v { v[:creation]}                          ,
+	plays:    -> k,v { -v[:total_count]}                      ,
+	finished: -> k,v { -v[:played].to_f/v[:total_tracks].to_f}
+}
 
 YAML_FILE   =File.expand_path "~/Music/playcount.yaml" 
 DIR_LOCATION="/Users/bilalh/Movies/add"
@@ -21,9 +26,26 @@ def creation_time(file)
                           "-raw", file)[1].read)
 end
 
-sorter = SORT_NAME
+# option parser
+ops = Trollop::options do
+	opt :name      , "Sort by name"
+	opt :tracks    , "Sort by total tracks"
+	opt :creation  , "Sort by creation date"
+	opt :plays     , "Sort by play counts"
+	opt :finished  , "Sort by percents played"
+end
+
+sorter = Sorts[:name]
+Sorts.each_key do |k|
+	if ops[k] then
+		sorter = Sorts[k]
+		break
+	end
+end
 
 hash = YAML::load File.open YAML_FILE 
+
+# Getting the data
 
 arr=hash.to_a.map do |e|
 	[e[0].gsub(%r{/Users/bilalh/Movies/add/}, '')
@@ -35,8 +57,6 @@ arr.each do |track|
 	name  = track[0][/(.*?)\//,1]
 	
 	album = albums[name] || {total_count:0, total_tracks:0,played:0}
-	# album = albums[name] || {tracks:[], total_count:0, total_tracks:0,played:0}
-	# album[:tracks] << track
 	album[:total_count] += track[1]
 	
 	if album[:total_tracks] = 0 then
@@ -47,6 +67,7 @@ arr.each do |track|
 	albums[name] = album
 end
 
+# Printing
 
 printf  "%-51s %4s  %4s %3s %s\n", "Album", "Tracks", "Plays", "Played?", "Date"
 albums.sort_by { |k,v| sorter[k,v] }.each do |e|
